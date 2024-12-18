0d564e850682ea5d8ea0fd76ece97956cd9f33a8
sudoris <zdent09@gmail.com>
2024-12-18 09:23:10 -0600

    use markdown-it-footnote to render markdown footnotes as links

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -18,13 +18,17 @@
           {{ articleAttributes.abstract }}
         </div>
 
-        <vue-markdown-it :source="markdown" class="article-body" />
+        <vue-markdown-it 
+          :source="markdown" 
+          class="article-body" 
+          :options="options" 
+          :plugins="[MarkdownItFootnote]" />
       </div>
       <div class="meta">
-        <div class="toc pa-5">Table of Contents</div>
-        <div class="attachments pa-5">Attachments</div>
-        <div class="related pa-5">Related</div>
-        <div class="tags pa-5">Tags</div>
+        <div class="toc my-5 ml-5 pa-5 d-flex justify-center">Table of Contents</div>
+        <div class="attachments my-5 ml-5 pa-5 d-flex justify-center">Attachments</div>
+        <div class="related my-5 ml-5 pa-5 d-flex justify-center">Related</div>
+        <div class="tags my-5 ml-5 pa-5 d-flex justify-center">Tags</div>
       </div>
     </v-container>
   </div>
@@ -34,6 +38,13 @@
 import { ref, onMounted, computed } from 'vue'
 import api from '@/api/index.js'
 import { VueMarkdownIt } from '@f3ve/vue-markdown-it'
+import MarkdownItFootnote from 'markdown-it-footnote'
+
+const options = {
+  html: true,
+  linkify: true,
+  footnote: true
+}
 
 const route = useRoute()
 
@@ -63,11 +74,7 @@ onMounted(async () => {
 
 }
 
-.split-article {
-  display: flex;
-}
-
-.related-section {
-
+.meta {
+  min-width: 15%;
 }
 </style>
